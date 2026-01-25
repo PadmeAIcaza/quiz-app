5016af89c3a265ed770d68f34824e21eeeb62728
spicypusheen <padme.garrapata@gmail.com>
2026-01-24 23:41:30 -0600

    Keep score + end-of-quiz functionality

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ parameters = {
     'amount':10,
     'type':'boolean'
 }
-response = requests.get(url='https://opentdb.com/api.php?amount=10&category=9&difficulty=hard&type=boolean', params=parameters)
+response = requests.get(url='https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean', params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = data['results']
