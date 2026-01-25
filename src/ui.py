69e6ab35ccbec195cc56aefe610feb3be7957866
spicypusheen <padme.garrapata@gmail.com>
2026-01-24 22:23:55 -0600

    Showing questions functionality

--- src/ui.py
+++ src/ui.py
@@ -1,17 +1,19 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 BG = "#DDF7E3"
 FONT = ('Times New Roman', 20, 'italic')
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title('Quizzzzler')
         self.window.config(padx=20, pady=20, bg=BG)
 
         # Text
         self.canvas = Canvas(width=300, height=250, bg='white')
-        self.question = self.canvas.create_text(150, 125, text="Question", fill='black', font=FONT)
+        self.question = self.canvas.create_text(150, 125, width=280, text="Question", fill='black', font=FONT)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         # Label
@@ -27,4 +29,10 @@ class QuizInterface:
         self.false_button = Button(self.window, image=self.false, bg=BG)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question, text=q_text)
