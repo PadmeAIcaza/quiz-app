fe4dc7aa134b5d3458b0e79236786f3ffe4a24fa
spicypusheen <padme.garrapata@gmail.com>
2026-01-24 23:10:08 -0600

    Feedback functionality

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from warnings import resetwarnings
+
 from quiz_brain import QuizBrain
 
 BG = "#DDF7E3"
@@ -13,8 +15,8 @@ class QuizInterface:
 
         # Text
         self.canvas = Canvas(width=300, height=250, bg='white')
-        self.question = self.canvas.create_text(150, 125, width=280, text="Question", fill='black', font=FONT)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        self.question = self.canvas.create_text(150, 125, width=280, text="Question", fill='black', font=FONT)
 
         # Label
         self.score_label = Label(self.window, text='Score: ', font=FONT, bg=BG, fg = 'black')
@@ -22,11 +24,11 @@ class QuizInterface:
 
         # Buttons
         self.true = PhotoImage(file='../images/T.png')
-        self.true_button = Button(self.window, image=self.true, bg=BG)
+        self.true_button = Button(self.window, image=self.true, bg=BG, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         self.false = PhotoImage(file='../images/F.png')
-        self.false_button = Button(self.window, image=self.false, bg=BG)
+        self.false_button = Button(self.window, image=self.false, bg=BG, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -34,5 +36,24 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question, text=q_text)
+
+    def true_pressed(self):
+       is_right = self.quiz.check_answer('True')
+       self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer('False')
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg='#A7D477')
+        else:
+            self.canvas.config(bg='#B33030')
+
+        self.window.after(1000, self.get_next_question)
+
+
